c92d55a9229cd5182fb956d5c4f5a487f9a35580
tcottrell321 <57600443+tcottrell321@users.noreply.github.com>
2020-02-19 12:49:07 -0800

    added circle marker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,8 +10,23 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+  //  Add a marker to the map for Los Angeles, California. Code for 
+  // pin marker followed by code for circle marker, followed by code for
+  // circleMarker function
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//L.circleMarker([34.0522, -118.2437]).addTo(map);
+//L.circle([34.0522, -118.2437], {
+//  radius: 100
+//}).addTo(map);
+L.circleMarker([34.0522, -118.2437], {
+  radius:300,
+  color: 'black',
+  fillColor: '#ffffa1'
+}).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_Key
