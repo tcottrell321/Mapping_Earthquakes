3e0b8e60aeb8b25071b8c44da8c960a81cd1d73d
Thomas Cottrell <57600443+tcottrell321@users.noreply.github.com>
2020-02-19 14:08:38 -0800

    Added Multiple Points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -10,13 +10,28 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
+//Get data from cities.js
+let cityData = cities;
+
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius:city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+ });
+
+
   //Add circle marker using the circleMarker function with some parameters
-  L.circleMarker([34.0522, -118.2437], {
-    radius:300,
-    color: 'black',
-    fillColor: '#ffffa1'
-  }).addTo(map);
-  
+ // L.circleMarker([34.0522, -118.2437], {
+  //  radius:300,
+  //  color: 'black',
+  //  fillColor: '#ffffa1'
+  //}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
